5e81a9c411a6cd61bc5acc1983bf8704deb7c64a
DenisWm <denismamoni@hotmail.com>
2022-12-14 17:01:05 -0400

    creating service and sending messages

---
+++ src/main/java/com/microservico/estoquepreco/service/RabbitmqService.java
@@ -0,0 +1,17 @@
+package com.microservico.estoquepreco.service;
+
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitmqService {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    public void enviarMensagem(String nomeFila, Object mensagem){
+        this.rabbitTemplate.convertAndSend(nomeFila, mensagem);
+    }
+}
